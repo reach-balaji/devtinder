9382d96084b9950b0a474df27390fc121cf60ee1
reach-balaji <reachbalajikariyappa87@gmail.com>
2026-05-21 11:16:04 +0530

    removed submodule and added src as normal folder

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express=require("express");
+
+const app=express();
+
+app.use("/hello",(req,res)=>{
+  res.send('Hello from server')
+})
+
+app.use("/test",(req,res){
+  res.send('tinder app')
+})
+app.listen(3000,()=>{
+
+    console.log("listening 3000");
+})
