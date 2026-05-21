3684615a77d6e4a822a279ccfbebb2aeb4c51aa2
reach-balaji <reachbalajikariyappa87@gmail.com>
2026-05-21 14:01:10 +0530

    fix: removed submodule and added src as normal folder

--- src/app.js
+++ src/app.js
@@ -4,12 +4,13 @@ const app=express();
 
 app.use("/hello",(req,res)=>{
   res.send('Hello from server')
-})
+});
 
-app.use("/test",(req,res){
+app.use("/test",(req,res)=>{
   res.send('tinder app')
 })
+
 app.listen(3000,()=>{
 
     console.log("listening 3000");
-})
+});
