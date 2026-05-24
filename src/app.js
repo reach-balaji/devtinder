0b82afa2dabe1a46d54123dc7b9664d0374365d8
reach-balaji <reachbalajikariyappa87@gmail.com>
2026-05-24 10:27:22 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@ app.get("/test",(req,res)=>{
   res.send('tinder app')
 })
 
-app.post("/",(req,res)=>{
-  res.send("hello")
-})
+// app.post("/",(req,res)=>{
+//   res.send("hello")
+// })
 
 app.get("/user",(req,res)=>{
   console.log(req.query)
@@ -22,7 +22,12 @@ app.get("/user",(req,res)=>{
     })
 })
 
-app.get("/users/:userId/:passowrd",(req,res)=>{
+app.get("/usersQuery",(req,res)=>{
+  console.log(req.query);
+  res.send("query params")
+})
+
+app.get("/users/:userId/:password",(req,res)=>{
   console.log(req.params)
   res.send(
     {firsrName:"Balaji",
@@ -30,7 +35,7 @@ app.get("/users/:userId/:passowrd",(req,res)=>{
     })
 })
 
-app.use("/hello",(req,res)=>{
+app.use("/",(req,res)=>{
   res.send('Hello from server')
 });
 
